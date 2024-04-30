93696abc0ecb455571dc116b903d85feb941f4ac
Nicolette <nicolette.chalup@ucb.edu.bo>
2024-04-30 01:41:34 -0400

    hacemos una operacion basica de sumar los puntajes y dividir por la cantidad como criterio basico(que sera cambiado por un criterio mas acorde al proyecto)

--- src/totalizador.js
+++ src/totalizador.js
@@ -2,7 +2,9 @@ let puntaje = 0;
 
 function calcularPuntaje(cantidad,cantidadPruebas, cantidadLineas, cobertura) {
     
-   puntaje = cantidad + cantidadPruebas + cantidadLineas + cobertura;
+   // Lógica para calcular el puntaje
+   puntaje = (cantidad + cantidadPruebas + cantidadLineas + cobertura) / 4;
+   ;
 }
 function obtenerPuntaje() {
   return puntaje;
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -7,10 +7,13 @@ describe("Totalizador", () => {
     expect(calcularPuntaje(10)).toEqual(10);
   });
   */
-  it("debería sumar el todas los datos correctamente ", () => {
+  /*it("debería sumar el todas los datos correctamente ", () => {
     calcularPuntaje(10, 20, 30, 40);
     expect(obtenerPuntaje()).toEqual(100);
+  });*/
+  it("debería calcular el puntaje correctamente con el criterio basico de sumar y dividir por la cantidad(sera cambiado proximamente por otro criterio)", () => {
+    calcularPuntaje(10, 20, 30, 40);
+    expect(obtenerPuntaje()).toEqual(25);
   });
- 
 });
 
